af41062621413ad301ae6a6b8fc8a9b356838d0e
SeanrPeters <58159948+SeanrPeters@users.noreply.github.com>
2023-12-04 17:16:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,40 @@
 
 
-// Initialize the map on window load
-window.onload = function() {
-  var map = L.map('map').setView([41.1490, -81.3419], 15);
-  initializeMap(map);
-  loadListings(map);
-};
+// event listeners, and dropdown interactions
+document.addEventListener("DOMContentLoaded", function() {
 
-function initializeMap(map) {
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-}
+    // Event listener for submitting a new listing
+    document.getElementById('listingForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const formData = {
+            Description: document.getElementById('description').value,
+            Price: document.getElementById('price').value,
+            Title: document.getElementById('title').value,
+            Photo_Urls: document.getElementById('photos').value,
+        };
+    
+        fetch('http://localhost:1433/api/listings', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
 
-document.getElementById('listingForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-  const formData = {
-      Description: document.getElementById('description').value,
-      Price: document.getElementById('price').value,
-      Title: document.getElementById('title').value,
-      Photo_Urls: document.getElementById('photos').value,
-  };
-
-  fetch('http://localhost:1433/api/listings', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-  })
-
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+    });
 });
 
+function initializeMap(map) {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
