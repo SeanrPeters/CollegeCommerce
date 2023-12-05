38a779d28eefc25c0c8f89adbf0648a94f75f4c1
SeanrPeters <58159948+SeanrPeters@users.noreply.github.com>
2023-12-04 23:56:04 -0500

    Update mapPins.js

--- mapPins.js
+++ mapPins.js
@@ -1,21 +1,25 @@
-// Placeholder for fetching data from a JSON file
+// Get listings from database
 function loadListings(map) {
-    fetch('listings.json')
-        .then(response => response.json())
-        .then(data => {
-            // Add listings to the map
-            addListingsToMap(map, data.listings);
-        })
-        .catch(error => {
-            console.error("Error fetching listings:", error);
-        });
+    fetch('http://localhost:1433/api/listings', {
+      method: 'GET',
+    })
+
+    .then(response => response.json())
+    .then(data => {
+        console.log("Retieved listings data.")
+        // Add listings to the map
+        addListingsToMap(map, data);
+    })
+    .catch(error => {
+         console.error("Error fetching listings:", error);
+    });
 }
 
 // Loop through listings and add markers to the map
 function addListingsToMap(map, listings) {
     listings.forEach(function(listing) {
         var popupContent = createPopupContent(listing);
-        var marker = L.marker(listing.coords).addTo(map);
+        var marker = L.marker([listing.Latitude, listing.Longitude]).addTo(map);
         marker.bindPopup(popupContent);
         marker.on('click', function(e) {
             L.DomEvent.stopPropagation(e); 
@@ -25,13 +29,15 @@ function addListingsToMap(map, listings) {
 }
 
 // Create the HTML content for a listing's popup
+// TODO use image from database
+// TODO make the link actually go to a dynamic listing page
 function createPopupContent(listing) {
     return `
       <div class="listing-popup">
-          <h3>${listing.title}</h3>
-          <img src="${listing.photoUrls[0]}" alt="${listing.title}" width="100" onclick="openModal(this)" />
-          <div class="price">${listing.price}</div>
-          <a href="${listing.url}" style="text-decoration: none;"><button>More Info</button></a>
+          <h3>${listing.Title}</h3>
+          <img src="\defaultImage.png" alt="${listing.Title}" width="100" onclick="openModal(this)" />
+          <div class="price">$${listing.Price}</div>
+          <a href="\listing.html" style="text-decoration: none;"><button>More Info</button></a>
       </div>
     `;
 }
